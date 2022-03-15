deb0e3ab367c81b0d8fba7c90b2b466e3b198fd7
lyn <76365499@qq.com>
2022-03-15 14:13:33 +0800

    useWindowSize

---
+++ src/core/useWindowSize/index.ts
@@ -0,0 +1,22 @@
+import { reactive, onMounted, onUnmounted } from 'vue'
+const useWindowSize = () => {
+    const state = reactive({
+        width: window.innerWidth,
+        height: window.innerHeight
+    })
+    const handler =() => {
+        state.width = window.innerWidth
+        state.height = window.innerHeight
+    }
+    onMounted(() => {
+        window.addEventListener('resize', handler)
+        // on(window, 'resize', handler) 此处应该统一事件监听
+    })
+    onUnmounted(() => {
+        window.removeEventListener('resize', handler)
+        //off(window, 'resize', handler) 此处应该统一事件监听
+    })
+    return state
+}
+
+export default useWindowSize
