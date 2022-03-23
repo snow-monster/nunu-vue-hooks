f78ff186eb594f3e3007c80e077e5faf24a43cf5
lyn <76365499@qq.com>
2022-03-23 15:37:21 +0800

    remove useThrottle test code

--- src/core/useThrottle/index.ts
+++ src/core/useThrottle/index.ts
@@ -1,6 +1,5 @@
 import { ref } from 'vue'
 
-
 function useThrottle (fn: Function, ms: number = 300) {
    // 上一次执行 fn 的时间
     const previous = ref(0)
@@ -17,10 +16,4 @@ function useThrottle (fn: Function, ms: number = 300) {
     }
 }
 
-
-const a = useThrottle(() => {
-    console.log('a')
-},3000)
-
-setInterval(() => a(), 10)
 export default useThrottle
